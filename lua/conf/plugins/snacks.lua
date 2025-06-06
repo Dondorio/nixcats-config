f6428aed965b3c6124b6f7a67f691c9305eb80ee
Donda <dondadecahedron@gmail.com>
2025-06-06 19:47:30 +0200

    Changed date display to display the day in waybar

--- lua/conf/plugins/snacks.lua
+++ lua/conf/plugins/snacks.lua
@@ -23,7 +23,7 @@ require("snacks").setup({
 
   indent = {
     priority = 1000,
-    enabled = true,     -- enable indent guides
+    enabled = true, -- enable indent guides
     char = "│",
     only_scope = false, -- only show indent guides of the scope
     only_current = false, -- only show indent guides in the current window
